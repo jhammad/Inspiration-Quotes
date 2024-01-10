0346ee50484ae46b5373e6394cff92f958037db0
jhammad <javier.hammad@gmail.com>
2024-01-10 13:31:32 +0000

    Add quotes and import StoreKit

--- InspoQuotes/QuoteTableViewController.swift
+++ InspoQuotes/QuoteTableViewController.swift
@@ -1,15 +1,10 @@
-//
-//  QuoteTableViewController.swift
-//  InspoQuotes
-//
-//  Created by Angela Yu on 18/08/2018.
-//  Copyright © 2018 London App Brewery. All rights reserved.
-//
-
 import UIKit
+import StoreKit // framework  of the StoreKit
 
 class QuoteTableViewController: UITableViewController {
     
+    let productID = "string of the productID of the developer"
+
     var quotesToShow = [
         "Our greatest glory is not in never falling, but in rising every time we fall. — Confucius",
         "All our dreams can come true, if we have the courage to pursue them. – Walt Disney",
@@ -30,85 +25,61 @@ class QuoteTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+     
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    // MARK: - Table view data source - to show the quotes
+    // assign number of cells
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        // return number of quotes
+        return quotesToShow.count + 1
     }
-
-    /*
+    // show quotes in the cells
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        // cell with identifier of the cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "QuoteCell", for: indexPath)
+        // if there are less cells than the number of rows
+        if indexPath.row < quotesToShow.count{
+            // assign the text to the cell
+            cell.textLabel?.text = quotesToShow[indexPath.row]
+            // assign the number of lines to 0 in case is needed more than 1 line of text
+            cell.textLabel?.numberOfLines = 0
+        }
+        else {
+            cell.textLabel?.text = "Get more quotes" // if there are more text for the last cell
+            cell.textLabel?.textColor = UIColor.blue // change the colour
+            cell.accessoryType = .detailDisclosureButton // to indicate that can be clicked
+        }
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    // MARK: - Table view delegate method - to click in the last cell
+    // function of the last cell
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == quotesToShow.count {
+            buyPremiumQuotes()
+        }
+        // deselected the row once clicked
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     
-    
-    
-    
+    // MARK: In-app purchases method
+    // fucntion to buy Premium Quotes 
+    func buyPremiumQuotes(){
+        // class of StoreKit to make sure that the user can make payments
+        if SKPaymentQueue.canMakePayments() {
+            // create new variable with the class SKMutablePayment of StoreKit
+            let paymentRequest = SKMutablePayment()
+            // assign the product identifier to our productID
+            paymentRequest.productIdentifier = productID
+        }
+        else {
+            print("User can't make payments")
+        }
+    }
+  
+
     @IBAction func restorePressed(_ sender: UIBarButtonItem) {
         
     }
